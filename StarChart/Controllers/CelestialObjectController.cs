c8a46f95161b34f82ea07276185f275b60927cc1
Dave van Klink <dave.vanklink@iddinkgroup.com>
2022-08-15 14:52:35 +0200

    Changes

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context { get; set; }
+        
+        [Route("")]
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+    }
+}
